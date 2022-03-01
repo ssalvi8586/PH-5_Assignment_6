cd81d9fee3ff6d2010aab6995976ce75e93237aa
ssalvi8586 <ssalvi8586@gmail.com>
2022-03-01 13:24:29 +0600

    result shown initially

--- js/home.js
+++ js/home.js
@@ -19,6 +19,18 @@ const searchResult = (data) => {
     data.data.forEach(element => {
         const phoneNamme = element.phone_name;
         const brand = element.brand;
-        console.log(brand);
+        const image = element.image;
+        const div = document.createElement("div");
+        div.classList.add("col-lg-4", "col-md-6", "col-12");
+        const parentContainer = document.getElementById('result-container');
+        div.innerHTML = `<div class="card">
+            <img src="${image}" class="card-img-top img-fluid" alt="... ">
+            <div class="card-body ">
+                <h5 class="card-title ">${phoneNamme}</h5>
+                <p class="card-text ">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="# " class="btn btn-primary ">Go somewhere</a>
+            </div>
+        </div>`
+        parentContainer.appendChild(div);
     });
 }
