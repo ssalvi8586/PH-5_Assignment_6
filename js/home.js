45216d8cf97ebfd7a7de85425c498ba40646659e
ssalvi8586 <ssalvi8586@gmail.com>
2022-03-01 13:09:17 +0600

    test div and instruction added

--- js/home.js
+++ js/home.js
@@ -1,6 +1,24 @@
 document.getElementById("searchBtn").addEventListener('click', () => {
+
     const searchText = document.getElementById("searchInput").value;
-    fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
-        .then(response => response.json())
-        .then(data => console.log(data.data[0]));
+
+    if (searchText != "") {
+
+        document.getElementById("instruction").style.display = "none";
+
+        fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+            .then(response => response.json())
+            .then(data => searchResult(data));
+    } else {
+        document.getElementById("instruction").style.display = "block";
+
+    }
 })
+
+const searchResult = (data) => {
+    data.data.forEach(element => {
+        const phoneNamme = element.phone_name;
+        const brand = element.brand;
+        console.log(brand);
+    });
+}
